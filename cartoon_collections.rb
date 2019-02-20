488bd85b50f078b6e38af296b3667be4347254e1
Natasha Chernis <natashachernis@gmail.com>
2019-02-20 06:01:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,12 @@ end
 def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  cheese_types.each do |cheese_type|
+    if ingredients.include?(cheese_type)
+      return cheese_type
+    else
+      return nil
+    end
+  end
 
 end
